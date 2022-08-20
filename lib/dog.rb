5fa8c6a662d6d910ca2a0d0597d130f13618a483
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-20 21:25:17 +0000

    commit tree fc1944e19839b08e0c9d240cad3950b9327bc96f

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting" 
+    end
+end
+
+#instances
+dook = Dog.new
+dook.bark
+sook = Dog.new
+sook.bark
+dook.sit
+sook.sit
